7f2ad2c5738d4ed2123e9518352da30e30ffa3ed
phen <yusukekitagawa7640@gmail.com>
2025-02-17 17:29:50 +0700

    added piece .svg's

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 const phessboard = document.getElementById("boardDIV");
 const letterArr = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const letterNumbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
-const squareStyle = `width: 64px; height: 64px; border: none; margin: 0px`;
+const squareStyle = `color: transparent; width: 64px; height: 64px; border: none; margin: 0px;`;
 
 let squareCreate;
 let rankCreate;
@@ -16,6 +16,7 @@ for (let rankDivs = 8; rankDivs > 0; rankDivs--) {
 	rankCreate = document.createElement("div");
 	rankCreate.setAttribute("id", `${rankDivs}`);
 	rankCreate.setAttribute("class", `${rankDivs}`);
+	rankCreate.setAttribute("style", `text-align: center;`);
 	phessboard.appendChild(rankCreate);
 
 	letterArr.forEach((eachLetter) => {
@@ -23,23 +24,14 @@ for (let rankDivs = 8; rankDivs > 0; rankDivs--) {
 		squareCreate.setAttribute("id", `${eachLetter}${rankDivs}`);
 		squareCreate.setAttribute("class", `${letterArr.indexOf(eachLetter)}`);
 		squareCreate.setAttribute("data-color", `white`);
-		squareCreate.setAttribute(
-			"style",
-			`background-color: white; color: transparent; ${squareStyle}`
-		);
+		squareCreate.setAttribute("style", `background-color: rgb(207, 223, 205); ${squareStyle}`);
 
 		if (squareCreate.className % 2 == 1 && rankCreate.className % 2 == 0) {
 			squareCreate.setAttribute("data-color", `black`);
-			squareCreate.setAttribute(
-				"style",
-				`background-color: black; color: transparent; ${squareStyle}`
-			);
+			squareCreate.setAttribute("style", `background-color: rgb(68, 75, 67); ${squareStyle}`);
 		} else if (squareCreate.className % 2 == 0 && rankCreate.className % 2 == 1) {
 			squareCreate.setAttribute("data-color", `black`);
-			squareCreate.setAttribute(
-				"style",
-				`background-color: black; color: transparent; ${squareStyle}`
-			);
+			squareCreate.setAttribute("style", `background-color: rgb(68, 75, 67); ${squareStyle}`);
 		}
 		rankCreate.appendChild(squareCreate);
 	});
